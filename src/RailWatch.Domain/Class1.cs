9c8e3688c13f30e9eefefdd748420dc3047980f7
Ben Sinyard <benjaminsinyard@gmail.com>
2026-02-01 20:09:54 +0000

    feat: Add Base Projects

---
+++ src/RailWatch.Domain/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace RailWatch.Domain;
+
+public class Class1
+{
+
+}
---
+++ src/RailWatch.Worker/Program.cs
@@ -0,0 +1,7 @@
+using RailWatch.Worker;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
